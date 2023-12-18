af180b81eefd47f43213f0cf91ffe120961c758c
Вяселева Динара <118040854+VyaselevaDinara@users.noreply.github.com>
2023-12-18 20:49:18 +0300

    Update pid.cs

--- pid.cs
+++ pid.cs
@@ -1,28 +1,28 @@
-﻿using System.Diagnostics;
+﻿using System.Diagnostics; // Подключаем необходимые пространства имен
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main(string[] args)  // Главная точка входа в программу
     {
-        AllInfoProcess();
+        AllInfoProcess(); // Вызываем метод, который выводит информацию о процессах
 
         static void AllInfoProcess()
         {
-            var myProcess = from proc in Process.GetProcesses(".")
+            var myProcess = from proc in Process.GetProcesses(".")    // Получаем все запущенные процессы, сортируем по идентификатору
                             orderby proc.Id
                             select proc;
-            Console.WriteLine("\n*** Текущие процессы ***\n");
+            Console.WriteLine("\n*** Текущие процессы ***\n");  // Выводим заголовок
             int flag = 0;
             foreach (var p in myProcess)
             {
-                if (flag < 3)
+                if (flag < 3) // Выводим информацию только для первых 3-х процессов
                     Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
                 flag++;
             }
 
             Console.WriteLine("-----------------------------------------------");
 
-            foreach (var p in myProcess)
+            foreach (var p in myProcess) // Выводим информацию о всех процессах
                 Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
         }
     }
